804c40ea0c3de1d3733c953f54cc0b55e00ae36f
DyegoJustino <91922439+DyegoJustino@users.noreply.github.com>
2022-05-02 21:07:56 -0300

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt-get install git ansible -y
+
+git clone https://github.com/DyegoJustino/pointer-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app/
+ansible-playbook pointer-playbook.yml
